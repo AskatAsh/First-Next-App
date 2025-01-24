be1a8dc40675f0ea3c2079075d0db86e2ed46776
AskatAsh <abdulaskat@gmail.com>
2025-01-24 22:26:30 +0600

    added min-height to blogpost page and wraped inside main tag

--- app/blogs/[id]/page.js
+++ app/blogs/[id]/page.js
@@ -1,27 +1,29 @@
 import { notFound } from "next/navigation";
 
-export default async function BlogPost({params}) {
-    const {id} = params;
+export default async function BlogPost({ params }) {
+  const { id } = params;
 
-    if(id > 100){
-        notFound();
+  if (id > 100) {
+    notFound();
+  }
+
+  const fetchBlogs = async () => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch blog posts.");
     }
 
-    const fetchBlogs = async () => {
-        const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-    
-        if (!res.ok) {
-          throw new Error("Failed to fetch blog posts.");
-        }
-    
-        const data = await res.json();
-        return data;
-      };
-      const blogPost = await fetchBlogs();
+    const data = await res.json();
+    return data;
+  };
+  const blogPost = await fetchBlogs();
   return (
-    <div className="my-6 space-y-4 max-w-2xl mx-auto w-11/12 border border-gray-900 rounded-md p-4">
+    <main className="my-6 min-h-screen">
+      <div className="space-y-4 max-w-2xl mx-auto w-11/12 border border-gray-900 rounded-md p-4">
         <h1 className="text-2xl font-semibold">{blogPost.title}</h1>
         <p>{blogPost.body}</p>
-    </div>
-  )
+      </div>
+    </main>
+  );
 }
